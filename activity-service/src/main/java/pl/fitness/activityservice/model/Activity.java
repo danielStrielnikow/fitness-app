9f19b41d94cca226f653850ff03e4f2335699b16
Daniel <daniel.strielnikow@outlook.com>
2025-04-20 16:16:49 +0200

    Add activity-microservice with mongoDB

---
+++ activity-service/src/main/java/pl/fitness/activityservice/model/Activity.java
@@ -0,0 +1,36 @@
+package pl.fitness.activityservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
+@Document(collection = "activities")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Activity {
+    private Long id;
+    private Long userId;
+    private  ActivityType type;
+    private Integer duration;
+    private Integer caloriesBurned;
+    private LocalDateTime startTime;
+
+    @Field("metrics")
+    private Map<String, Object> additionalMetrics;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
---
+++ activity-service/src/main/java/pl/fitness/activityservice/model/ActivityType.java
@@ -0,0 +1,14 @@
+package pl.fitness.activityservice.model;
+
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    CYCLING,
+    SWIMMING,
+    WEIGHT_TRAINING,
+    YOGA,
+    HITI,
+    CARDIO,
+    STRETCHING,
+    OTHER
+}
