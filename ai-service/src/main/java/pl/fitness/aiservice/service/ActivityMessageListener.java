a99a830e281f6d542f389e45170714132d9a1664
Daniel <daniel.strielnikow@outlook.com>
2025-04-24 21:31:24 +0200

    Ad RabbitMQ to AiService adn Activity

---
+++ ai-service/src/main/java/pl/fitness/aiservice/service/ActivityMessageListener.java
@@ -0,0 +1,19 @@
+package pl.fitness.aiservice.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+import pl.fitness.aiservice.model.Activity;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ActivityMessageListener {
+
+
+    @RabbitListener(queues = "activity.queue")
+    public void processActivity(Activity activity) {
+        log.info("Received activity for processing: {}", activity.getId());
+    }
+}
