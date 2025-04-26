140d91148fa3ea9ce0138cacce8033cc28c6ae68
Daniel <daniel.strielnikow@outlook.com>
2025-04-26 21:53:55 +0200

    Precessing Recommendations

--- ai-service/src/main/java/pl/fitness/aiservice/service/ActivityMessageListener.java
+++ ai-service/src/main/java/pl/fitness/aiservice/service/ActivityMessageListener.java
@@ -5,15 +5,22 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 import pl.fitness.aiservice.model.Activity;
+import pl.fitness.aiservice.model.Recommendation;
+import pl.fitness.aiservice.repo.RecommendationRepository;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class ActivityMessageListener {
 
+    private final ActivityAIService aiService;
+    private final RecommendationRepository recommendationRepository;
 
     @RabbitListener(queues = "activity.queue")
     public void processActivity(Activity activity) {
         log.info("Received activity for processing: {}", activity.getId());
+
+        Recommendation recommendation = aiService.generateRecommendation(activity);
+        recommendationRepository.save(recommendation);
     }
 }
